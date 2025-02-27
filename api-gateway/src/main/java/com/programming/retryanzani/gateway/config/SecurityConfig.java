38a2911a1bc06ce8c5b72c572843418f7f8a9597
retrodf <114569570+retrodf@users.noreply.github.com>
2025-02-27 17:01:50 +0700

    Part 8

--- api-gateway/src/main/java/com/programming/retryanzani/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/programming/retryanzani/gateway/config/SecurityConfig.java
@@ -11,16 +11,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", 
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated())
+                    .requestMatchers(freeResourceUrls)
+                    .permitAll()
+                    .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
-
-    // @Bean
-    // public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
-    //     return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
-    // }
 }
